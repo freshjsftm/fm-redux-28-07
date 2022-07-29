49a166258f89b42653d84a639b6fbe94fed52da4
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-29 10:23:45 +0300

    Add tasksCreators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,45 @@ import ACTION_TYPES from "../actions/actionTypes";
 const initialState = {
   count: 0,
   step: 1,
-  newProps:true
+  tasks:[
+    {
+      id:0,
+      body:'test task',
+      isDone: false
+    }
+  ]
 };
-
+let serial = 1;
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case ACTION_TYPES.CREATE_TASK:{
+      const {values} = action;
+      const {tasks} = state;
+      return {
+        ...state,
+        tasks: [...tasks, {...values, id:serial++}]
+      }
+    }
+    case ACTION_TYPES.UPDATE_TASK:{
+      const {id, values} = action;
+      const {tasks} = state;
+      const newTasks = tasks.map((task)=>{
+        if(task.id===id){
+          return {...task, ...values};
+        }
+        return task;
+      })
+      return {
+        ...state,
+        tasks: newTasks
+      };
+    }
+    case ACTION_TYPES.DELETE_TASK:{
+      return {
+        ...state,
+        tasks: state.tasks.filter((task)=>task.id!==action.id)
+      }
+    }
     case ACTION_TYPES.INCREMENT: {
       return {
         ...state,
