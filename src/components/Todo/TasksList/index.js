0ea8e97d9c3b6a077a082f2ada4c8594997526a6
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-29 11:24:19 +0300

    Add TasksList

--- src/components/Todo/TasksList/index.js
+++ src/components/Todo/TasksList/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { updateTask, deleteTask } from "../../../actions/taskCreators";
 
-const TasksList = () => {
+const TasksList = (props) => {
+  const { tasks, updateTaskDispatch, deleteTaskDispatch } = props;
   return (
-    <div>
-      
-    </div>
+    <section>
+      <h2>list tasks</h2>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div style={{ border: "2px solid teal", width: "50%" }}>
+              <p>
+                id:{task.id}
+                <input
+                  type="checkbox"
+                  checked={task.isDone}
+                  onChange={({ target: { checked } }) =>
+                    updateTaskDispatch(task.id, { isDone: checked })
+                  }
+                />
+                <button onClick={() => deleteTaskDispatch(task.id)}>
+                  Delete
+                </button>
+              </p>
+              <h3>{task.body}</h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
-}
+};
+const mapStateToProps = ({ tasks }) => ({ tasks });
+const mapDispatchToProps = (dispatch) => ({
+  updateTaskDispatch: (id, values) => dispatch(updateTask(id, values)),
+  deleteTaskDispatch: (id) => dispatch(deleteTask(id)),
+});
 
-export default TasksList;
+export default connect(mapStateToProps, mapDispatchToProps)(TasksList);
