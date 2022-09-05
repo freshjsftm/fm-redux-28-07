c5b798ff364f18fe0d1fc73a4cd48fbb8e689c29
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-09-05 09:46:11 +0300

    Add bindActionCreators

--- src/components/Users/UserList/index.js
+++ src/components/Users/UserList/index.js
@@ -1,33 +1,38 @@
-import React, {useEffect} from 'react';
-//import { connect } from 'react-redux';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from "react";
+import { bindActionCreators } from "redux";
+import { useSelector, useDispatch } from "react-redux";
 import * as ActionsUserCreators from "../../../actions/userCreators";
 
 const UserList = (props) => {
-  const {users, isFetching, error} = useSelector(({usersState})=>usersState);
-  const dispatch = useDispatch();
-  const getUsersRequestDispatch = ({limit, offset})=>dispatch(ActionsUserCreators.getUsersRequest({limit, offset}))
-  //const {users, isFetching, error, getUsersRequestDispatch} = props;
-  const loadMore = ()=>getUsersRequestDispatch({offset:users.length});
-  //useEffect(()=>{getUsersRequestDispatch({offset:users.length})}, [])
-  useEffect(()=>{loadMore()}, [])
+  const { users, isFetching, error } = useSelector(
+    ({ usersState }) => usersState
+  );
+  //const dispatch = useDispatch();
+  const {getUsersRequest} = bindActionCreators(ActionsUserCreators, useDispatch());
+  // const getUsersRequestDispatch = ({ limit, offset }) =>
+  //   dispatch(ActionsUserCreators.getUsersRequest({ limit, offset }));
+  const loadMore = () => getUsersRequest({ offset: users.length });
+  useEffect(() => {
+    loadMore();
+  }, []);
   return (
     <section>
       <h2>Users List</h2>
       <button onClick={loadMore}>Load more</button>
       {isFetching && <p>Loading...</p>}
-      {error && <p>{error.message}</p>} 
-      <ul style={{maxHeight:'60vh', overflow:'auto', border:'3px solid teal'}}>
-        {
-          users.map((user)=>(<li key={user.id}>{user.email}</li>))
-        }
+      {error && <p>{error.message}</p>}
+      <ul
+        style={{
+          maxHeight: "60vh",
+          overflow: "auto",
+          border: "3px solid teal",
+        }}
+      >
+        {users.map((user) => (
+          <li key={user.id}>{user.email}</li>
+        ))}
       </ul>
     </section>
   );
-}
+};
 export default UserList;
-// const mapStateToProps = ({usersState})=>usersState;
-// const mapDispatchToProps = (dispatch)=>({
-//   getUsersRequestDispatch: ({limit, offset}) => dispatch(ActionsUserCreators.getUsersRequest({limit, offset}))
-// })
-// export default connect(mapStateToProps, mapDispatchToProps)(UserList);
